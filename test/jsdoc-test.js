a3fcaeac2fe083da0102809b5379b098973212a9
likaixuan <likaixuan@hexin.im>
2023-07-13 14:58:43 +0800

    init: 项目初始化 增加部分规则

---
+++ test/jsdoc-test.js
@@ -0,0 +1,14 @@
+function foo() {
+  return 1;
+}
+
+const obj = {
+  b: 1,
+  a: 3
+};
+console.log("obj")
+foo();
+
+const obj2 = {
+  a:1,
+  b:2,c:3,V:4,sd:5,sadasdasdasdjagsjhbhahjdhs:9, lSAJDKJHJBDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDjsadhksdbv:'8999fsdfsdfsdfsdfsdfdddddddddddddddddddddddddddd999999999'}
---
+++ test/test.vue
@@ -0,0 +1,8 @@
+<template>
+  <div v-for="item in 10">
+    <span>{{ item }}</span>
+  </div>
+</template>
+<script>
+console.log(123, '123');
+</script>
